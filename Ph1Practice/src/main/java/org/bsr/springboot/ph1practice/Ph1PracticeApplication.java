333a10fecb703914eaf550999798fbd6b3dbafc0
finn_the_human <sbenjamin726@outlook.com>
2025-12-25 15:38:59 -0600

    Starting Phase1 Practice Project. Goal: Be comfortable building CRUD apps with REST endpoints and database integration.

---
+++ Ph1Practice/src/main/java/org/bsr/springboot/ph1practice/Ph1PracticeApplication.java
@@ -0,0 +1,13 @@
+package org.bsr.springboot.ph1practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ph1PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ph1PracticeApplication.class, args);
+    }
+
+}
---
+++ Ph1Practice/src/test/java/org/bsr/springboot/ph1practice/Ph1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsr.springboot.ph1practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ph1PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
