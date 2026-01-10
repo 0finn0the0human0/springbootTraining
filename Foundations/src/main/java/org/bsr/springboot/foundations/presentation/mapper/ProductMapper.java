eeba7b725dc83f08bca0fdd701329d2bd3dfde3e
finn_the_human <sbenjamin726@outlook.com>
2026-01-09 23:02:38 -0600

    Added ProductMapper class to map dto's and added ProductMapper to ProductService

---
+++ Foundations/src/main/java/org/bsr/springboot/foundations/presentation/mapper/ProductMapper.java
@@ -0,0 +1,41 @@
+/***********************************************************************************************************************
+ * Project: Core Spring Boot Foundations - Phase 1
+ * Description: The ProductMapper is a manual mapper class that manually map the ProductRequest and ProductResponse dto
+ *              fields without the use of a third party library for training purposes. Otherwise the recommended
+ *              approach would be to use MapStruct - a powerful, annotation-based code generator that automatically
+ *              creates efficient, boilerplate-free mapping code at compile time.
+ * Author: Benjamin Soto-Roberts
+ * Created: 01/09/26
+ * */
+
+package org.bsr.springboot.foundations.presentation.mapper;
+
+
+import org.bsr.springboot.foundations.persistence.entity.Product;
+import org.bsr.springboot.foundations.presentation.dto.ProductResponseDTO;
+import org.springframework.stereotype.Component;
+
+
+// Uses Component stereotype for bean management
+@Component
+public class ProductMapper {
+
+
+    /**
+     * This method takes in a product, does a null check and maps the product fields to the appropriate
+     * ProductResponseDTO fields and returns a response dto.
+     * */
+    public ProductResponseDTO toResponseDto(Product product) {
+        if  (product == null) {
+            return null;
+        }
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getProductName(),
+                product.getProductDesc(),
+                product.getRetailPrice()
+        );
+    }
+
+
+}
--- Foundations/src/main/java/org/bsr/springboot/foundations/service/ProductService.java
+++ Foundations/src/main/java/org/bsr/springboot/foundations/service/ProductService.java
@@ -12,6 +12,7 @@ import org.bsr.springboot.foundations.persistence.entity.Product;
 import org.bsr.springboot.foundations.persistence.repository.ProductRepository;
 import org.bsr.springboot.foundations.presentation.dto.ProductRequestDTO;
 import org.bsr.springboot.foundations.presentation.dto.ProductResponseDTO;
+import org.bsr.springboot.foundations.presentation.mapper.ProductMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,16 +23,19 @@ public class ProductService {
 
     // Final for thread safety
     private final ProductRepository repository;
+    private final ProductMapper mapper;
 
     /*The ProductRepository is injected into the constructor. SpringBoot automatically injects dependencies into a class
      with a single constructor*/
-    public ProductService(ProductRepository repository) {
+    public ProductService(ProductRepository repository, ProductMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
+
 
     /**
      * The method takes in the client request and uses the abstract method created in the ProductRepository to check if
-     * the clients search term was found in any of the product names. The method maps the list of products to response
+     * the clients search term was found in any of the product names then maps the list of products to response
      * dtos to display back to the client.
      * */
     public List<ProductResponseDTO> getProductsContaining(ProductRequestDTO productRequest) {
@@ -40,11 +44,7 @@ public class ProductService {
         List<Product> products = repository.findAllByProductNameContainingIgnoreCase(searchTerm);
 
         return products.stream()
-                .map(product -> new ProductResponseDTO(
-                        product.getId(),
-                        product.getProductName(),
-                        product.getProductDesc(),
-                        product.getRetailPrice()))
+                .map(mapper::toResponseDto)
                 .collect(Collectors.toList());
     }
 
